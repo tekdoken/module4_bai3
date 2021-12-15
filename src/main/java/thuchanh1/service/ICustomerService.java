cd4c793da1da4091bec6190b4eb290191faf318f
tekdoken <tekdokenzx@gmail.com>
2021-12-15 10:06:37 +0700

    thuc hanh 1

---
+++ src/main/java/thuchanh1/service/ICustomerService.java
@@ -0,0 +1,18 @@
+package thuchanh1.service;
+
+
+import thuchanh1.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
